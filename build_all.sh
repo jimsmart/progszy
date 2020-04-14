7eaeb1fbb44862085c96a38605074dfdc0c0d9e2
Jim Smart <jim@jimsmart.org>
2020-04-14 03:00:04 +0100

    Cleanups

--- build_all.sh
+++ build_all.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-go build
 
-cd cmd/progszy
-go build
+# TODO This doesn't work properly on macOS.
 
-mkdir -p windows-amd64
-GOOS=windows GOARCH=amd64 go build -v -o windows-amd64/
+# go build
 
-mkdir -p linux-amd64
-GOOS=linux GOARCH=amd64 go build -v -o linux-amd64/
+# cd cmd/progszy
+# go build
 
-# mkdir -p linux-arm
-# GOOS=linux GOARCH=arm go build -v -o linux-arm/
+# # mkdir -p windows-amd64
+# # CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -v -o windows-amd64/
 
-# mkdir -p linux-386
-# GOOS=linux GOARCH=386 go build -v -o linux-386/
+
+
+# mkdir -p linux-amd64
+# CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -o linux-amd64/ -ldflags "-linkmode external -extldflags -static"
+# # ldflags come from https://github.com/mattn/go-sqlite3/issues/217
+
+
+# # mkdir -p linux-arm
+# # GOOS=linux GOARCH=arm go build -v -o linux-arm/
+
+# # mkdir -p linux-386
+# # GOOS=linux GOARCH=386 go build -v -o linux-386/
 
